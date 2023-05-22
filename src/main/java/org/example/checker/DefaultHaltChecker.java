752387ba94f0f9b03b97aab3f0fc01d6216e3543
rodri <rodrigomatamorosbecerra@gmail.com>
2023-05-22 18:48:41 +0200

    Commit 2

--- src/main/java/org/example/checker/DefaultHaltChecker.java
+++ src/main/java/org/example/checker/DefaultHaltChecker.java
@@ -7,6 +7,41 @@ public class DefaultHaltChecker implements HaltChecker {
         // Aquí deberías implementar la lógica de análisis del programa P con la entrada I
         // y determinar si el programa se detendrá o no. Retornar "para" o "nunca" en consecuencia.
         // Por simplicidad, asumiré que siempre retorna "nunca" en este ejemplo.
+
+        /*
+        * // Lógica para verificar si el programa se detiene con la entrada dada
+    boolean[][] haltTable = {
+        {true, true, true, false},  // Ejemplo de tabla de parada (debe ser adaptada según las reglas de tu lenguaje)
+        {true, true, true, false},
+        {true, true, true, false},
+        {false, false, false, true}
+    };
+
+    int state = 0;  // Estado inicial
+    int index = 0;  // Índice para recorrer la entrada
+
+    while (true) {
+        char currentChar = program.charAt(index);  // Carácter actual del programa
+
+        // Obtener la acción según el estado y el carácter actual
+        boolean haltAction = haltTable[state][Character.getNumericValue(currentChar)];
+
+        if (haltAction) {
+            return "nunca";  // El programa no se detiene
+        }
+
+        // Actualizar el estado y el índice
+        state = (state + 1) % haltTable.length;
+        index = (index + 1) % program.length();
+
+        // Verificar si hemos alcanzado el final del programa
+        if (index == 0) {
+            return "para";  // El programa se detiene
+        }
+    }
+}
+
+      * */
         return "nunca";
     }
 }
